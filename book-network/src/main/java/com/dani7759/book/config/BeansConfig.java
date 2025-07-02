62bb4fed182cb0b796d6f102f7ecb05cc16e657f
Daniel7759 <danieljh7759@gmail.com>
2025-07-02 12:54:09 -0500

    Add dynamic allowedOrigins using application-dev.yml to set list of origins and change allowed headers and methods for test deploy

--- book-network/src/main/java/com/dani7759/book/config/BeansConfig.java
+++ book-network/src/main/java/com/dani7759/book/config/BeansConfig.java
@@ -1,6 +1,7 @@
 package com.dani7759.book.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -17,6 +18,7 @@ import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import static org.springframework.http.HttpHeaders.*;
 
@@ -25,6 +27,8 @@ import static org.springframework.http.HttpHeaders.*;
 public class BeansConfig {
 
     private final UserDetailsService userDetailsService;
+    @Value("${application.cors.origins:*}")
+    private List<String> allowedOrigins;
 
     @Bean
     public AuthenticationProvider authenticationProvider(){
@@ -53,21 +57,10 @@ public class BeansConfig {
     public CorsFilter corsFilter() {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
-        config.setAllowedHeaders(Arrays.asList(
-                ORIGIN,
-                CONTENT_TYPE,
-                ACCEPT,
-                AUTHORIZATION
-        ));
-        config.setAllowedMethods(Arrays.asList(
-                "GET",
-                "POST",
-                "DELETE",
-                "PUT",
-                "PATCH"
-        ));
+        //config.setAllowCredentials(true);
+        config.setAllowedOrigins(allowedOrigins);
+        config.setAllowedHeaders(Arrays.asList("*")); //not recommended for production
+        config.setAllowedMethods(Arrays.asList("*")); //not recommended for production
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
